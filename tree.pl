77001a70d7733e7d5fba29048143a8c8e188ee61
latkop <pauljlatkovic@gmail.com>
2022-04-10 03:39:54 -0600

    finished makeTree

--- tree.pl
+++ tree.pl
@@ -47,14 +47,14 @@ makeTree(Input, Output) :-
 makeTree(Input, Output) :-
     mainParse(Input, Res),
     Res == 1,
-    % makeTree2(Input, Answer),
+    makeTree2(Input, Answer),
     open(Output,write,Stream),
     write(Stream, Answer),
     close(Stream).
 
 
 % recursive function for creating tree
-makeTree2(Input, Answer) :-
+makeTree2(Input, Tree) :- % dont forget to revert Tree to Answ
     readMyFile(Input, Flines),
     delTrailingSpaces(Flines, [], Flines2),
     delMember('', Flines2, Nlines),
@@ -64,22 +64,108 @@ makeTree2(Input, Answer) :-
     parseMP(Nlines, 0, _, MP),
     parseTNP(Nlines, 0, _, TNP),
     tasks(Tasks),
-    makeTree3([FPA, FM, TNT, MP, TNP], 0, Score, [], Tree, Tasks),
-    treeToString(Tree, Score, Answer). % answer = Solution H G F E D C B A; Quality: 800
+    maxBound(Max),
+    makeTree3([FPA, FM, TNT, MP, TNP], [[], Max], [[], 0], Tasks, Tree).
+    % treeToString(Tree, Score, Answer). % answer = Solution H G F E D C B A; Quality: 800
     
 % param: ([constraint lists], [[prev tree], prev score], [[curr tree], curr score], [remaining tasks], return)   
-% if current task is already used, test next task
-makeTree3(Aids, Prev, Curr, [T|Tx], Ans) :-
+
+% line 31 MTree.hs
+makeTree3(_, Prev, _, T, Ans) :-
+    length(T, Length),
+    Length == 0,
+    Ans = Prev.
+
+% line 33 MTree.hs, checking if task already in use
+makeTree3(Aids, Prev, Curr, [T|Ts], Ans) :-
+    write(Prev), write("    "), write(Curr), write(T), write("\n"), 
     nth0(0, Curr, CPath),
     member(T, CPath),
-    makeTree3(Aids, Prev, Curr, Tx, Ans).
+    makeTree3(Aids, Prev, Curr, Ts, Ans).
 
-
-% makeTree3(Aids, Prev, Curr, [T|Tx], Ans) :-
+% line 34 MTree.hs, checking hard constraints
+makeTree3(Aids, Prev, Curr, [T|Ts], Ans) :-
+    nth0(0, Curr, CPath),
+    nth0(1, Curr, _),
+    appendElem(CPath, T, CPath2),
+    doTests(CPath2, Aids, Pen),
+    Pen < 0,
+    makeTree3(Aids, Prev, Curr, Ts, Ans).
     
-%[[],[],['11111111','11111111'],[],[]]
+% line 35 MTree.hs
+makeTree3(Aids, Prev, Curr, [T|_], Ans) :-
+    nth0(0, Curr, CPath),
+    nth0(1, Curr, CScore),
+    nth0(1, Prev, PScore),
+    appendElem(CPath, T, CPath2),
+    doTests(CPath2, Aids, Pen),
+    Pen2 is Pen + CScore,
+    Pen2 > PScore,
+    Ans = Prev.
+
+% line 36 MTree.hs
+makeTree3(Aids, Prev, Curr, [T|_], Ans) :-
+    nth0(0, Curr, CPath),
+    nth0(1, Curr, CScore),
+    nth0(1, Prev, PScore),
+    appendElem(CPath, T, CPath2),
+    doTests(CPath2, Aids, Pen),
+    length(CPath2, Length),
+    Length >= 8,
+    Pen2 is Pen + CScore,
+    Pen2 < PScore,
+    Ans = [CPath2, Pen2].
+
+% line 37 MTree.hs
+makeTree3(Aids, Prev, Curr, [T|_], Ans) :-
+    nth0(0, Curr, CPath),
+    nth0(1, Curr, CScore),
+    nth0(1, Prev, PScore),
+    appendElem(CPath, T, CPath2),
+    doTests(CPath2, Aids, Pen),
+    length(CPath2, Length),
+    Length >= 8,
+    Pen2 is Pen + CScore,
+    Pen2 >= PScore,
+    Ans = [CPath2, CScore].
+
+% line 38 MTree.hs
+makeTree3(Aids, Prev, Curr, [T|Ts], Ans) :-
+% snd (makeTree2 aids prev (((fst curr)++[x]), ((snd curr) + doTests ((fst curr)++[x]) aids)) tasks) < (snd prev)
+    nth0(0, Curr, CPath),
+    nth0(1, Curr, CScore),
+    nth0(1, Prev, PScore),
+    appendElem(CPath, T, CPath2),
+    doTests(CPath2, Aids, Pen),
+    Pen2 is Pen + CScore,
+    tasks(Tasks),
+    makeTree3(Aids, Prev, [CPath2, Pen2], Tasks, Ans1),
+    nth0(1, Ans1, AScore),
+    AScore < PScore,
+    makeTree3(Aids, Ans1, Curr, Ts, Ans).
+    % makeTree2 aids (makeTree2 aids prev (((fst curr)++[x]), ((snd curr) + doTests ((fst curr)++[x]) aids)) tasks) curr xs
+
+% line 39 MTree.hs
+makeTree3(Aids, Prev, Curr, [T|Ts], Ans) :-
+    %snd (makeTree2 aids prev (((fst curr)++[x]), ((snd curr) + doTests ((fst curr)++[x]) aids)) tasks) >= (snd prev) 
+    nth0(0, Curr, CPath),
+    nth0(1, Curr, CScore),
+    nth0(1, Prev, PScore),
+    appendElem(CPath, T, CPath2),
+    doTests(CPath2, Aids, Pen),
+    Pen2 is Pen + CScore,
+    tasks(Tasks),
+    makeTree3(Aids, Prev, [CPath2, Pen2], Tasks, Ans1),
+    nth0(1, Ans1, AScore),
+    AScore >= PScore,
+    makeTree3(Aids, Prev, Curr, Ts, Ans).
+    %= makeTree2 aids prev curr xs
+    
 minBound(X) :-
     X = -123456789.
+
+maxBound(X) :-
+    X = 123456789.
 
 % do all constraint tests
 
@@ -134,7 +220,7 @@ doTests(Path, Cnst, Pen) :-
 % line 47 MTree.hs
 doTests(Path, Cnst, Pen) :-
     length(Path, M),
-    SL = M - 1,         % second last index
+    SL is M - 1,         % second last index
     nth1(M, Path, T),   % last task
     nth1(SL, Path, SLT),% second last task
     atom_concat(SLT, T, TT),
@@ -145,34 +231,42 @@ doTests(Path, Cnst, Pen) :-
 doTests(Path, Cnst, Pen) :-
     length(Path, M),
     M == 8,
-    SL = M - 1,         % second last index
+    SL is M - 1,         % second last index
     nth1(M, Path, T),   % last task
     nth1(SL, Path, SLT),% second last task
     nth1(1, Path, F),
     nth0(3, Cnst, MP),
     nth0(4, Cnst, TNP),
-    atom_concat(M3, T, MT),
+    atom_concat(M, T, MT),
     machPen(MT, MP, P1),
     tooNearPen(SLT, T, TNP, P2),
     tooNearPen(T, F, TNP, P3),
-    Pen = P1 + P2 + P3.
+    Pen is P1 + P2 + P3.
 
 % line 48 MTree.hs
 doTests(Path, Cnst, Pen) :-
     length(Path, M),
-    SL = M - 1,         % second last index
+    SL is M - 1,         % second last index
     nth1(M, Path, T),   % last task
     nth1(SL, Path, SLT),% second last task
     nth0(3, Cnst, MP),
     nth0(4, Cnst, TNP),
-    atom_concat(M3, T, MT),
+    atom_concat(M, T, MT),
     machPen(MT, MP, P1),
     tooNearPen(SLT, T, TNP, P2),
-    Pen = P1 + P2.
+    Pen is P1 + P2.
     
-
-    
+% Tree to string
+% treeToString
 
  
 testTests(File, Path, Pen) :-
-    
+    readMyFile(File, Flines),
+    delTrailingSpaces(Flines, [], Flines2),
+    delMember('', Flines2, Nlines),
+    parseFPA(Nlines, 0, _, X),
+    parseFM(Nlines, 0, _, X2),
+    parseTNT(Nlines, 0, _, X3),
+    parseMP(Nlines, 0, _, X4),
+    parseTNP(Nlines, 0, _, X5),
+    doTests(Path, [X, X2, X3, X4, X5], Pen).
